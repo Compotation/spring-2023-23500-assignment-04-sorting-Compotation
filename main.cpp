fd603e63e2d1f9cfbdf0ec6eebc69bec6d642346
Compotation <46853874+Compotation@users.noreply.github.com>
2023-04-16 17:23:08 -0400

    random numbers

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include "sort.h"
 #include<iostream>
+#include <random>
 
 int main() {
-  std::vector<int> vec = {3,6,-1,8, 2, -100, 2, 30, -20, 30, -1000, 100};
+  std::vector<int> vec = {};
+  std::random_device rd; // create a random device to seed the generator
+  std::mt19937 gen(rd()); // create a Mersenne Twister engine and seed it with rd
+
+  std::uniform_int_distribution<> dis(-100000000, 1000000000); // create a uniform distribution between 1 and 100
+
+  for (int i = 0; i < 50000; i++) {
+    int random_integer = dis(gen);
+    vec.push_back(random_integer);
+  }
+  // {37, 21, 14, 6, 50, 9, 28, 42, 16, 33, 47, 19, 3, 38, 12, 45, 31, 55, 77, 2, 91, 63, 10, 24, 98, 13, 40, 27, 83, 57, 18, 75, 22, 94, 41, 67, 7}
   // 2, -100, 2, 30, -20, 30, -1000, 100
   /*
   std::vector<int> vec1 = {7, 0, 9, 6, 5};
@@ -14,7 +25,7 @@ int main() {
 qsort2(vec, 0, vec.size()-1);
 //std::cout << vec.size() << "\n";
 for (int i = 0; i < vec.size(); i++) {
-  std::cout << vec[i] << ",";
+  std::cout << vec[i] << ", ";
 }
 std::cout << std::endl;
   return 0;
