099bea7cdd79b09fe14bd7eff6290911cb8414a1
Compotation <46853874+Compotation@users.noreply.github.com>
2023-04-16 17:25:18 -0400

    check if ordered

--- sort.cpp
+++ sort.cpp
@@ -2,7 +2,7 @@
 #include<iostream>
 
 std::vector<int> qsort2(std::vector<int> &vec, int low, int high) {
-  std::cout << "params: " << low << " | " << (int) ((low + high) / 2) << " | " << high << "\n";
+//  std::cout << "params: " << low << " | " << (int) ((low + high) / 2) << " | " << high << "\n";
   // base case
   // changed to < to sort subarray of 2
   if (high - low < 1) {
@@ -13,16 +13,16 @@ std::vector<int> qsort2(std::vector<int> &vec, int low, int high) {
   // for now we'll just pick list[0]
   int pivotIndex = medianIndex(vec, low, (int) ((low + high) / 2), high);
   int pivot = vec[pivotIndex];
-  std::cout << "pivot index: " << pivotIndex << " pivot: " << pivot << "\n";
+//  std::cout << "pivot index: " << pivotIndex << " pivot: " << pivot << "\n";
 
 
   // copy all the values < pivot to lower
   // copy all the values >= pivot to higher;
   // should check last index
   //make separate loops for each pivot type
-  std::cout << "pivot: ";
+//  std::cout << "pivot: ";
   if (pivotIndex == low) {
-    std::cout << "low\n";
+//    std::cout << "low\n";
     for (int i = low + 1; i <= high; i++) {
       if (vec[i] < pivot) {
         int value = vec[i];
@@ -32,7 +32,7 @@ std::vector<int> qsort2(std::vector<int> &vec, int low, int high) {
       }
     }
   } else if (pivotIndex == high) {
-    std::cout << "high\n";
+//    std::cout << "high\n";
     for (int i = low; i < pivotIndex; i++) {
       if (vec[i] > pivot) {
         int value = vec[i];
@@ -42,10 +42,10 @@ std::vector<int> qsort2(std::vector<int> &vec, int low, int high) {
       }
     }
   } else {
-    std::cout << "mid\n";
+//    std::cout << "mid\n";
     for (int i = low; i <= high; i++) {
       if (i == pivotIndex) {
-        std::cout << "skipped pivot";
+//        std::cout << "skipped pivot";
       } else if (vec[i] < pivot && i > pivotIndex) {
         int value = vec[i];
         vec.erase(vec.begin() + i);
@@ -61,11 +61,11 @@ std::vector<int> qsort2(std::vector<int> &vec, int low, int high) {
       }
     }
   }
-  std::cout << "current run: \n";
-  for (int i = 0; i < vec.size(); i++) {
-    std::cout << vec[i] << " , ";
-  }
-  std::cout << "\n";
+//  std::cout << "current run: \n";
+//  for (int i = 0; i < vec.size(); i++) {
+//    std::cout << vec[i] << " , ";
+//  }
+//  std::cout << "\n";
 
   // make our recursive calls
   // lower
@@ -89,4 +89,13 @@ int medianIndex(std::vector<int> &vec, int firstIndex, int middleIndex, int last
   } else {
     return lastIndex;
   }
+}
+
+bool is_vector_ordered(const std::vector<int> &v) {
+  for (std::vector<int>::size_type i = 1; i < v.size(); i++) {
+    if (v[i] < v[i-1]) {
+      return false;
+    }
+  }
+  return true;
 }
